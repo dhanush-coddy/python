c99ab9f16ad41427a9adbb5d80b934e33f6c462d
Dhanush A <adhanush596@gmail.com>
2025-05-21 12:07:14 +0530

    Initial commit

--- fi2.py
+++ fi2.py
@@ -1,2 +1,21 @@
-file=open("ex.txt","r")
+# Reading from the file (optional)
+file = open("ex.txt", "r")
 print(file.read())
+file.close()
+
+# Writing to the file
+file = open(r"C:\Users\Ethnotech\Desktop\python\ex.txt", "w")
+file.write("\n hello")
+
+file.writelines([
+    "hello\n",
+    "sjdksajdk\n",
+    "saldalskmdklasd\n",
+    "dwjkjsndn"
+])
+file.close()
+
+# Reading again to see changes
+file = open(r"C:\Users\Ethnotech\Desktop\python\ex.txt", "r")
+print(file.read())
+file.close()
